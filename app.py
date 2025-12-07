9ee39f87a64b0f8dd467586311c149c4627ecf61
AlysiaMutyaba <alishamutyaba4@gmail.com>
2025-12-07 12:56:41 +0300

    Added a requirement

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ load_model()
 load_coffee_leaf_detector()
 
 app = Flask(__name__)
-app.secret_key = "dbuefueueio27436yrubdyhgfwjt2jbhgdhdg"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file
